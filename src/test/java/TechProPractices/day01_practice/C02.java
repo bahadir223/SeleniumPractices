322e783298a35620977a38882c26868ba645e418
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-06-04 14:22:21 +0300

    updated

--- src/test/java/TechProPractices/day01_practice/C02.java
+++ src/test/java/TechProPractices/day01_practice/C02.java
@@ -18,29 +18,55 @@ public class C02 {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
 
-        // pencereyi kapatiniz
-
-        // https://www.amazon.com/ adresine gidin
+        //https://www.amazon.com/ adresine gidin
         driver.get("https://www.amazon.com/");
         // arama motorunda nutella yazip aratınız
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("nutella", Keys.ENTER);
 
-        // Customer Service ve Registry butonlarına sırasıyla tıklayın ve title'larının "Amazon" icerdigini test edin
-        List<WebElement> butonlar = driver.findElements(By.xpath("(//*[@class='nav-a  '])[position()>=2 and position()<=3]"));
-/*
-        for (WebElement w : butonlar) {
-            butonlar = driver.findElements(By.xpath("(//*[@class='nav-a  '])[position()>=2 and position()<=3]"));
-            w.click();
-            Thread.sleep(3000);
-            driver.navigate().back();
-
-        }*/
-        for (int i = 0; i <butonlar.size() ; i++) {
-            butonlar = driver.findElements(By.xpath("(//*[@class='nav-a  '])[position()>=2 and position()<=3]"));
-            butonlar.get(i).click();
+        //Customer Service ve Registry butonlarına sırasıyla tıklayın ve title'larının "Amazon" icerdigini test edin.
+        List<WebElement> linkler = driver.findElements(By.xpath("(//*[@class='nav-a  '])" +
+                "[position()>=2 and position()<=3]"));
+        //   FORİ İLE position()>= kullanarak .... ÇALIŞTI.
+        for (int i = 0; i < linkler.size(); i++) {
+            linkler = driver.findElements(By.xpath("(//*[@class='nav-a  '])" +
+                    "[position()>=2 and position()<=3]"));
+            linkler.get(i).click();
+            if (driver.getTitle().contains("Amazon")) {
+                System.out.println("Test PASSED");
+            } else {
+                System.out.println("Test FAILED");
+            }
             Thread.sleep(3000);
             driver.navigate().back();
         }
+
+        /*
+        FOREACH ile position()>= kullanarak .... ÇALIŞMADI.
+        for(WebElement w : linkler){
+            linkler = driver.findElements(By.xpath("(//*[@class='nav-a  '])" +
+                    "[position()>=2 and position()<=3]"));
+            w.click();
+            if(driver.getTitle().contains("Amazon")){
+                System.out.println("Test PASSED");
+            }else{
+                System.out.println("Test FAILED");
+            }
+            Thread.sleep(3000);
+            driver.navigate().back();
+        }*/
+
+
+        // | (OR) yapısı kullanılarak FORİ ile. ÇALIŞTI...
+        /*
+        List<WebElement> butonlar = driver.
+                findElements(By.xpath("//*[@class='nav-a  '][2]|//*[@class='nav-a  '][3]"));
+        for (int i = 0; i < butonlar.size() ; i++) {
+            butonlar = driver.
+                    findElements(By.xpath("//*[@class='nav-a  '][2]|//*[@class='nav-a  '][3]"));
+            butonlar.get(i).click();
+            Thread.sleep(3000);
+            driver.navigate().back();
+        }*/
 
         driver.close();
 
