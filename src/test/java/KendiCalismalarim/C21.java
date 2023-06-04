15fb004f3e6d419e6e4130beb475adbc38c0791b
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-06-04 21:33:13 +0300

    updated

---
+++ src/test/java/KendiCalismalarim/C21.java
@@ -0,0 +1,66 @@
+package KendiCalismalarim;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C21 {
+    /*
+    1. Launch browser
+    2. Navigate to url 'http://automationexercise.com'
+    3. Verify that home page is visible successfully
+    4. Click on 'Signup / Login' button
+    5. Verify 'Login to your account' is visible
+    6. Enter correct email address and password
+    7. Click 'login' button
+    8. Verify that 'Logged in as username' is visible
+    9. Click 'Delete Account' button
+    10. Verify that 'ACCOUNT DELETED!' is visible
+     */
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
+        //Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+        String actualURL = driver.getCurrentUrl();
+        String expectedURL = "https://automationexercise.com/";
+        if (actualURL.equals(expectedURL)) {
+            System.out.println("Test PASSED");
+        } else {
+            System.out.println("Test FAILED " + actualURL);
+        }
+
+        //Click on 'Signup / Login' button
+        driver.findElement(By.cssSelector("a[href='/login']")).click();
+
+        //Verify 'Login to your account' is visible
+        boolean b1 = driver.findElement(By.xpath("(//h2)[1]")).isDisplayed();
+        System.out.println(b1);
+
+        //Enter correct email address and password
+
+        driver.findElement(By.cssSelector("input[data-qa='login-email']")).
+                sendKeys("ali1@gmail.com", Keys.TAB, "12345", Keys.TAB, Keys.ENTER);
+
+        //Verify that 'Logged in as username' is visible
+        System.out.println(driver.findElement(By.xpath("//*[text()=' Logged in as ']")).isDisplayed());
+
+        //Click 'Delete Account' button
+        driver.findElement(By.cssSelector("a[href='/delete_account']")).click();
+
+        //Verify that 'ACCOUNT DELETED!' is visible
+        System.out.println(driver.findElement(By.cssSelector("h2[class='title text-center']")).isDisplayed());
+
+        driver.close();
+
+
+    }
+}
--- src/test/java/TechProPractices/day01_practice/C02.java
+++ src/test/java/TechProPractices/day01_practice/C02.java
@@ -1,9 +1,6 @@
 package TechProPractices.day01_practice;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -24,9 +21,29 @@ public class C02 {
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("nutella", Keys.ENTER);
 
         //Customer Service ve Registry butonlarına sırasıyla tıklayın ve title'larının "Amazon" icerdigini test edin.
-        List<WebElement> linkler = driver.findElements(By.xpath("(//*[@class='nav-a  '])" +
-                "[position()>=2 and position()<=3]"));
+        try{
+            List<WebElement> linkler = driver.findElements(By.xpath("(//*[@class='nav-a  '])" +
+                    "[position()>=2 and position()<=3]"));
+        }catch (StaleElementReferenceException e){
+            List<WebElement> linkler = driver.findElements(By.xpath("(//*[@class='nav-a  '])" +
+                    "[position()>=2 and position()<=3]"));
+            for (WebElement w : linkler) {
+
+                linkler = driver.findElements(By.xpath("(//*[@class='nav-a  '])" +
+                        "[position()>=2 and position()<=3]"));
+                w.click();
+                if (driver.getTitle().contains("Amazon")) {
+                    System.out.println("Test PASSED");
+                } else {
+                    System.out.println("Test FAILED");
+                }
+                Thread.sleep(3000);
+                driver.navigate().back();
+        }
+
+
         //   FORİ İLE position()>= kullanarak .... ÇALIŞTI.
+        /*
         for (int i = 0; i < linkler.size(); i++) {
             linkler = driver.findElements(By.xpath("(//*[@class='nav-a  '])" +
                     "[position()>=2 and position()<=3]"));
@@ -38,25 +55,33 @@ public class C02 {
             }
             Thread.sleep(3000);
             driver.navigate().back();
-        }
-
-        /*
-        FOREACH ile position()>= kullanarak .... ÇALIŞMADI.
-        for(WebElement w : linkler){
-            linkler = driver.findElements(By.xpath("(//*[@class='nav-a  '])" +
-                    "[position()>=2 and position()<=3]"));
-            w.click();
-            if(driver.getTitle().contains("Amazon")){
-                System.out.println("Test PASSED");
-            }else{
-                System.out.println("Test FAILED");
-            }
-            Thread.sleep(3000);
-            driver.navigate().back();
         }*/
 
 
-        // | (OR) yapısı kullanılarak FORİ ile. ÇALIŞTI...
+        //FOREACH ile position()>= kullanarak .... ÇALIŞMADI.
+/*
+        try {
+            for (WebElement w : linkler) {
+
+                linkler = driver.findElements(By.xpath("(//*[@class='nav-a  '])" +
+                        "[position()>=2 and position()<=3]"));
+                w.click();
+                if (driver.getTitle().contains("Amazon")) {
+                    System.out.println("Test PASSED");
+                } else {
+                    System.out.println("Test FAILED");
+                }
+                Thread.sleep(3000);
+                driver.navigate().back();
+
+
+            }
+        }*/ /*catch (StaleElementReferenceException e) {
+
+            }
+*/
+
+            // | (OR) yapısı kullanılarak FORİ ile. ÇALIŞTI...
         /*
         List<WebElement> butonlar = driver.
                 findElements(By.xpath("//*[@class='nav-a  '][2]|//*[@class='nav-a  '][3]"));
@@ -68,7 +93,8 @@ public class C02 {
             driver.navigate().back();
         }*/
 
-        driver.close();
+            driver.close();
 
+        }
     }
 }
