a95d60cfb3cfbf8888cbf9850f8ebb1f6c541a08
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-06-12 14:18:37 +0300

    C01_ActionsClass class'i oluşturuldu ve tamamlandı.

--- src/test/java/KendiCalismalarim/C01_ActionsClass.java
+++ src/test/java/KendiCalismalarim/C01_ActionsClass.java
@@ -1,16 +1,20 @@
 package KendiCalismalarim;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
 
 public class C01_ActionsClass {
     /*
-    Selenium'da Cift Tiklama, Sag tiklama nasil yapilir?
+    Selenium'da Cift Tiklama, Sag tiklama nasil yapilir? Örnek ile inceleyelim.
      */
 
     WebDriver driver;
@@ -25,7 +29,32 @@ public class C01_ActionsClass {
     }
 
     @Test
-    public void test01() throws InterruptedException {
-        //radiobutton elementlerini locate edin
+    public void test01() {
+        bekle(2);
+        //açılan sitedeki Double Click Me button'una tıklayınız.
+        WebElement doubleClickMeButton = driver.findElement(By.id("doubleClickBtn"));
+        Actions action = new Actions(driver);
+        action.doubleClick(doubleClickMeButton).perform();
+        //You have done a double click yazısının göründüğünü doğrulayınız.
+        WebElement doubleClickMessage = driver.findElement(By.id("doubleClickMessage"));
+        Assert.assertTrue(doubleClickMessage.isDisplayed());
+        bekle(2);
+        //Right Click Me button'una tıklayınız.
+        WebElement rightClickMeButton = driver.findElement(By.id("rightClickBtn"));
+        action.contextClick(rightClickMeButton).perform();
+        //You have done a right click yazısının göründüğünü doğrulayınız.
+        WebElement rightClickMessage = driver.findElement(By.id("doubleClickMessage"));
+        Assert.assertTrue(rightClickMessage.isDisplayed());
+
+
+    }
+
+
+    public void bekle(int saniye) {
+        try {
+            Thread.sleep(saniye * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
