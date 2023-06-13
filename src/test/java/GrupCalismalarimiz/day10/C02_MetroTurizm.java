f3689bcc224fe5e07b5e04d03b3c69e1ca39e3ba
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-06-13 17:28:15 +0300

    Metro Turizm projesine devam edildi.

--- src/test/java/GrupCalismalarimiz/day10/C02_MetroTurizm.java
+++ src/test/java/GrupCalismalarimiz/day10/C02_MetroTurizm.java
@@ -2,6 +2,7 @@ package GrupCalismalarimiz.day10;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -10,7 +11,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.sql.Date;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.util.List;
 
 public class C02_MetroTurizm {
     WebDriver driver;
@@ -49,15 +53,55 @@ public class C02_MetroTurizm {
 
         //gidis tarihine 15.06.2023 sec
         driver.findElement(By.xpath("(//*[@class='fa fa-calendar'])[1]")).click();
-        driver.findElement(By.xpath("(//*[@data-month='5'])[4]")).click();
-
+        WebElement secilenTarih = driver.findElement(By.xpath("//*[text()='15']"));
+        secilenTarih.click();
 
         //donus tarihini 18.06.2023 olarak isaretle
         driver.findElement(By.xpath("(//*[@class='fa fa-calendar'])[2]")).click();
-        driver.findElement(By.xpath("(//*[@data-month='5'])[4]")).click();
+        driver.findElement(By.xpath("//*[text()='18']")).click();
 
         //Listele'ye tıkla
         driver.findElement(By.id("btnIndexSearchJourneys")).click();
 
+        //Gidiş seferleri için en üstteki 3 butonun çalıştığını kontrol et
+
+
+        List<WebElement> secButonlari = driver.findElements(By.xpath("//*[@class='btn btn-select ngSelectJourney']"));
+        for (int i = 0; i < 3; i++) {
+            secButonlari = driver.findElements(By.xpath("//*[@class='btn btn-select ngSelectJourney']"));
+            secButonlari.get(i).click();
+            bekle(2);
+            WebElement koltukSecYazisi = driver.findElement(By.xpath("(//*[text()='Lütfen bir koltuk seçiniz'])[1]"));
+            System.out.println((i + 1) + " .Sefer = " + koltukSecYazisi.getText());
+            Assert.assertTrue(koltukSecYazisi.isDisplayed());
+            bekle(2);
+            //driver.findElement(By.xpath("(//*[text()='Kaldır'])[i+1]")).click();
+
+        }
+
+        List<WebElement> bosKoltuklar = driver.findElements(By.xpath("//*[@ng-if='!col.isSold']"));
+        for (int i = 0; i < 5; i++) {
+            bosKoltuklar = driver.findElements(By.xpath("//*[@ng-if='!col.isSold']"));
+            bosKoltuklar.get(i).click();
+            //System.out.println("Seçilen  = " + (i+1) + ".koltuk = " + bosKoltuklar.get(i).getText());
+        }
+        bekle(2);
+
+        //ekranda açılan alert penceresini kapat.
+        driver.switchTo().alert().accept();
+        //Dönüş seferi seç butonuna tıkla.
+        driver.findElement(By.xpath("(//*[@id='btnChooseReturnJourney'])[1]")).click();
+
+
+         //  locate ==>     (//*[@class="btn btn-select ngSelectJourneyReturn"])[1]
+
+    }
+
+    public void bekle(int saniye) {
+        try {
+            Thread.sleep(saniye * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
