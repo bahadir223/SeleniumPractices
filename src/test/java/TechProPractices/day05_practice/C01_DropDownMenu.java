8563bbbf88b187c3701b5ffb26ba04ce37587a93
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-06-14 21:01:57 +0300

    DropDownMenu practice örnegi yapildi.

--- src/test/java/TechProPractices/day05_practice/C01_DropDownMenu.java
+++ src/test/java/TechProPractices/day05_practice/C01_DropDownMenu.java
@@ -1,7 +1,31 @@
 package TechProPractices.day05_practice;
 
 import Utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 public class C01_DropDownMenu extends TestBase {
+    // https://www.amazon.com/ sayfasina gidin
+    // dropdown'dan "Books" secenegini secin
+    // arama cubuguna "Java" aratın
+    // arama sonuclarinin Java icerdigini test edin
 
+    @Test
+    public void test01() {
+        // https://www.amazon.com/ sayfasina gidin
+        driver.get("https://www.amazon.com/");
+        //dropdown'dan "Books" secenegini secin
+        WebElement ddm = driver.findElement(By.cssSelector("select[id='searchDropdownBox']"));
+        Select select = new Select(ddm);
+        select.selectByVisibleText("Books");
+        //arama cubuguna "Java" aratın
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Java", Keys.ENTER);
+        //arama sonuclarinin Java icerdigini test edin
+        String javaYazisi = driver.findElement(By.xpath("(//*[@class='sg-col-inner'])[1]")).getText();
+        Assert.assertTrue(javaYazisi.contains("Java"));
+    }
 }
