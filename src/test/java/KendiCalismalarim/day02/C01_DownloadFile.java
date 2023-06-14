a236b7bec1e1a80b9a0606966ec8f90be2bb790d
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-06-14 22:32:48 +0300

    C01_DownloadFile class'i olusturuldu.

--- src/test/java/KendiCalismalarim/day02/C01_DownloadFile.java
+++ src/test/java/KendiCalismalarim/day02/C01_DownloadFile.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 
 import java.io.File;
+import java.nio.file.Paths;
 
 public class C01_DownloadFile extends TestBase {
     /*
@@ -17,9 +18,7 @@ public class C01_DownloadFile extends TestBase {
         driver.get("https://demoqa.com/upload-download");
         //download button'una tiklayin.
         driver.findElement(By.id("downloadButton")).click();
-        String downloadPath = "C:\\Users\\bahad\\Downloads";
-        String fileName="sampleFile.jpeg";
+        bekle(5);
 
-        //File file = new File()
     }
 }
