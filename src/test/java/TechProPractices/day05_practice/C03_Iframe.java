8a52b6849b636b62cd455b1b03ee0b3b6f2e51d6
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-06-14 23:45:31 +0300

    iframe örnegi tamamlandı.

--- src/test/java/TechProPractices/day05_practice/C03_Iframe.java
+++ src/test/java/TechProPractices/day05_practice/C03_Iframe.java
@@ -4,6 +4,7 @@ import Utilities.TestBase;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 
@@ -19,15 +20,26 @@ public class C03_Iframe extends TestBase {
     public void test01() {
         // https://html.com/tags/iframe sayfasına gidiniz
         driver.get("https://html.com/tags/iframe");
+
         // Videoyu görecek kadar asagiya ininiz
         Actions actions = new Actions(driver);
         actions.sendKeys(Keys.PAGE_DOWN).sendKeys(Keys.PAGE_DOWN).perform();
+
+        /*
+        scroll'ü aşağıya indirme şu şekilde de yapılabilirdi:
+        // JavascriptExecutor örneği al
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        // Sayfayı 500 piksel aşağı kaydır
+        js.executeScript("window.scrollBy(0,500)");
+
+         */
+
         //Videoyu izlemek icin Play tusuna basiniz
         //bunun icin önce iframe'e geciş yapmamız lazım çünkü video, iframe nin icinde...
         driver.switchTo().frame(driver.findElement(By.xpath("(//iframe)[1]")));
         driver.findElement(By.cssSelector("[class='ytp-large-play-button ytp-button ytp-large-play-button-red-bg']")).click();
         //Videoyu calistirdiginizi test ediniz
-        //videoyu açtıktan sonra sağ altta youtube yazısı çıkıyor.onun locate'ini aldık...
+        //videoyu açtıktan sonra sağ altta youtube yazısı çıkıyor.onun locate'ini aldık...bu şekilde test ettik
         Assert.assertTrue(driver.findElement(By.xpath("//*[@class='ytp-youtube-button ytp-button yt-uix-sessionlink']")).isDisplayed());
 
         //'Powerful,but easy to misuse' yazısının gorunur oldugunu test ediniz
