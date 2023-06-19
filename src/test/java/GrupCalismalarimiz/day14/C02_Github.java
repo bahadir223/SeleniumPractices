eb63cf0e96782fbf5cd0138e8c42858c1d275401
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-06-20 01:17:32 +0300

    C02_Github class üzerinde calismalar bitirildi.

--- src/test/java/GrupCalismalarimiz/day14/C02_Github.java
+++ src/test/java/GrupCalismalarimiz/day14/C02_Github.java
@@ -14,17 +14,30 @@ public class C02_Github extends TestBase {
         driver.get("http://szimek.github.io/signature_pad/");
 
 //        - Çıkan ekrana istediğiniz çizgi yada şekli çiziniz
+
+        /*
+        moveByOffset :
+        fare işaretçisini belirtilen x ve y koordinatlarına göre belirtilen ofset değeri kadar hareket ettirir.
+         */
         WebElement ekran = driver.findElement(By.className("signature-pad--body"));
         Actions actions = new Actions(driver);
-        actions.moveToElement(ekran).clickAndHold(ekran).moveByOffset(100, 100)  // 100 birim sağa, 100 birim aşağı
-                .moveByOffset(-100, 0)   // 100 birim sola
-                .moveByOffset(0, -100)   // 100 birim yukarı
-                .moveByOffset(-100, 0)   // 100 birim sola
-                .moveByOffset(0, -100)   // 100 birim yukarı
-                .moveByOffset(100, 0).perform();
+        actions.clickAndHold(ekran).moveByOffset(100, 100)  // 100 birim sağa, 100 birim aşağı
+                .moveByOffset(-100, 0)
+                .moveByOffset(0, -100)
+                .moveByOffset(-100, 0)
+                .moveByOffset(0, -100)
+                .moveByOffset(100, 0).moveByOffset(40, 50).
+                moveByOffset(50, 60).
+                moveByOffset(60, 10).
+                moveByOffset(70, 15).
+                moveByOffset(80, 20).
+                moveByOffset(90, 30).release().perform();
+
+
+        bekle(6);
 
 //        - Çizimden sonra clear butonuna basınız
-        driver.findElement(By.className("button clear")).click();
+        driver.findElement(By.xpath("(//*[@type='button'])[1]")).click();
 //        - Sayfayi kapatiniz
 
     }
