bda359c9af6ab4b0240e458e68bdf0422506d069
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-07-04 23:39:09 +0300

    Excel veri okuma ve yazma genel tekrar calismalari yapildi.

---
+++ src/test/java/GrupCalismalarimiz/day21/C01_Odev.java
@@ -0,0 +1,17 @@
+package GrupCalismalarimiz.day21;
+
+public class C01_Odev {
+    /*
+        ODEV2
+        Yeni bir test method olusturalim writeExcelTest()
+        Adimlari takip ederek 1.satira kadar gidelim
+        5.hucreye yeni bir cell olusturalim
+        Olusturdugumuz hucreye "Nufus" yazdiralim
+        2.satir nufus kolonuna 1500000 yazdiralim
+        10.satir nufus kolonuna 250000 yazdiralim
+        15.satir nufus kolonuna 54000 yazdiralim
+        Dosyayi kaydedelim
+        Dosyayi kapatalim
+    */
+
+}
