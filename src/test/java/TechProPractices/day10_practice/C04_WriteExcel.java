50defb80f962667d319557fd87cffa970e9b243c
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-07-05 22:53:25 +0300

    WriteExcel calismasi yapildi.

---
+++ src/test/java/TechProPractices/day10_practice/C04_WriteExcel.java
@@ -0,0 +1,27 @@
+package TechProPractices.day10_practice;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class C04_WriteExcel {
+    // Ulkeler dosyasındaki 0. satır index'in, 4. hücre index'ine yeni bir cell olusturalım.
+    // olusturdugumuz hücreye "Nufus" yazdıralım.
+
+
+    @Test
+    public void test01() throws IOException {
+        FileInputStream fis = new FileInputStream("src/test/java/resources/ulkeler.xlsx");
+        Workbook workbook = WorkbookFactory.create(fis);
+        workbook.getSheet("Sayfa1").getRow(0).createCell(4).setCellValue("Nufus");
+        workbook.getSheet("Sayfa1").getRow(1).createCell(4).setCellValue(10);
+        FileOutputStream fos = new FileOutputStream("src/test/java/resources/ulkeler.xlsx");
+        workbook.write(fos);
+        fos.close();
+        workbook.close();
+    }
+}
