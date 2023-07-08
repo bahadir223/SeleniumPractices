df18b786d4676cc682b00aab2a79426a2578f7af
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-07-08 15:51:20 +0300

    ShadowRoot calismasi yapildi.

--- src/test/java/GrupCalismalarimiz/day21/C03_Calisma.java
+++ src/test/java/GrupCalismalarimiz/day21/C03_Calisma.java
@@ -4,14 +4,16 @@ import Utilities.TestBase;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
 
+public class C03_Calisma extends TestBase {
 
-
-public class C02_Mercedes extends TestBase {
-    //sayfaya gidin cerezi kapatin
-    //https://www.mercedes-benz.com.tr/?group=all&subgroup=see-all&view=BODYTYPE
-
-       /*
+    @Test
+    public void test01() {
+        //https://fiddle.luigi-project.io/#/home/wc1
+        driver.get("https://fiddle.luigi-project.io/#/home/wc1");
+        driver.findElement(By.xpath("//*[text()='Close']")).click();
+        /*
         shadowRoot neden kullanıyoruz??? ben locate aldığım halde eğer hiç locate çıkmıyorsayani 0 geliyorsa
         diyeceğiz ki bu işte bir iş var. sonra sayfada almak istediğimiz yere gidip üst taraflarına çıkacağız ve
         shadowRoot ariyacağız. sonra shadowRoot ün bir üstünde veya iki üstünde id numarası gibi bir şey oalcak
@@ -20,16 +22,10 @@ public class C02_Mercedes extends TestBase {
         diyerek cssSelector!!! ile........ normal alacağız. yine 0 çıkacak. sorun yok......
         Bu şekilde normalde locate ettiğimiz halde alamadığımız locate'leri alacağızz........
          */
-
-
-    @Test
-    public void test01() {
-        driver.get("https://www.mercedes-benz.com.tr/");
-        SearchContext shadowRoot = driver.findElement(By.tagName("cmm-cookie-banner")).getShadowRoot();
-        shadowRoot.findElement(By.cssSelector("[class='button button--accept-all wb-button hydrated']")).click();
-
-
+        SearchContext shadowRoot = driver.findElement(By.tagName("luigi-wc-2f77632f6c6973742e6a73")).getShadowRoot();
+        shadowRoot.findElement(By.cssSelector("[class=\"add-new-list-item-input\"]")).sendKeys("deneme");
+        WebElement privacy = driver.findElement(By.xpath("(//*[@class='fd-link svelte-1i4l3qp'])[1]"));
+        bekle(2);
+        privacy.click();
     }
-
-
 }
