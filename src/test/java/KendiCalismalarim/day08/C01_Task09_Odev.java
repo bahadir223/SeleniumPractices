1250c67c80bcc208a911edd7091afe2f911434b4
Bahadir Gunuvar <bahadirgunuvar223@gmail.com>
2023-07-09 23:31:28 +0300

    task09 task10 ve SoftAssert classlari olusturuldu.

---
+++ src/test/java/KendiCalismalarim/day08/C01_Task09_Odev.java
@@ -0,0 +1,62 @@
+package KendiCalismalarim.day08;
+
+import Utilities.TestBase;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class C01_Task09_Odev extends TestBase {
+    //https://www.selenium.dev/downloads/ adresine gidelim
+    //Sayfanın resmini alalım
+    //Platforms Supported by Selenium altındaki Browsers bölümü görünene kadar sayfayı indirelim
+    //Browser bölümünden Chrome driver bölümündeki documentation linkine tıklıyalım
+    //Documentation webelementinin resmini alalım
+    //All versions available in Downloads altında Latest stable release olan linki tıklayalım
+    //Açılan pencerede chromedriver'i indirelim
+    //Driver'in indiğini doğrulayalım
+    //İndirmiş olduğumuz dosyayı silelim
+    //Silindiğini doğrulayalım
+    //Not: Bu task'in her adımı için Html rapor oluşturalım(Extent Report)
+
+    ExtentReports extentReports;
+    ExtentHtmlReporter extentHtmlReporter;
+    ExtentTest extentTest;
+
+    @Test
+    public void test01() throws IOException {
+        extentReports = new ExtentReports();
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        String dosyaYolu = "testOutPut/extentReports/extentReport.html" + tarih + ".html";
+        extentHtmlReporter = new ExtentHtmlReporter(dosyaYolu);
+        extentReports.attachReporter(extentHtmlReporter); // --> HTML formatında raporlamayı başlatacak.
+
+        //Raporda gözükmesini istediğimiz bilgiler icin
+        extentReports.setSystemInfo("Browser", "Chrome");
+        extentReports.setSystemInfo("Tester", "Bahadir");
+        extentHtmlReporter.config().setDocumentTitle("Extent Report");
+        extentHtmlReporter.config().setReportName("Smoke Test Raporu");
+
+        extentTest = extentReports.createTest("Extend Test", "Test Raporu");
+
+        //https://www.selenium.dev/downloads/ adresine gidelim
+        driver.get("https://www.selenium.dev/downloads/");
+        extentTest.info("https://www.selenium.dev/downloads/ adresine gidildi");
+
+        //Sayfanın resmini alalım
+        String tarih2 = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        String dosyaYolu2 = "src/test/java/TumSayfaResmiSS/screenShot" + tarih2 + ".jpg";
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        FileUtils.copyFile(ts.getScreenshotAs(OutputType.FILE), new File(dosyaYolu2));
+        extentTest.info("https://www.selenium.dev/downloads/ sayfasının resmi alındı.");
+        //Platforms Supported by Selenium altındaki Browsers bölümü görünene kadar sayfayı indirelim
+
+    }
